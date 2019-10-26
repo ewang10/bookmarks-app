1c24c35c030a2e7c995cf692ac175b5dfc1cb64b
Eric Wang <ericwang27759@gmail.com>
2019-10-26 13:56:24 -0400

    cleaning...

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$Q2.nFua75q75DVpWgSUh4eS39dvYDKdMwIg.UecMoVPmziAFeI.6e',
+  API_KEY: process.env.REACT_APP_API_KEY,
 }
