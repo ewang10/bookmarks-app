d37d793d62a08397d46419676a39157a917578f7
Eric Wang <ericwang27759@gmail.com>
2019-10-26 13:06:12 -0400

    added proto types validation

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import AddBookmark from './AddBookmark/AddBookmark';
 import BookmarkList from './BookmarkList/BookmarkList';
+import BookmarksContext from './BookmarksContext';
 import Nav from './Nav/Nav';
 import config from './config';
 import './App.css';
+import Rating from './Rating/Rating';
 
 const bookmarks = [
   // {
@@ -32,7 +34,7 @@ const bookmarks = [
 
 class App extends Component {
   state = {
-    bookmarks,
+    bookmarks: [],
     error: null,
   };
 
@@ -47,6 +49,12 @@ class App extends Component {
     this.setState({
       bookmarks: [ ...this.state.bookmarks, bookmark ],
     })
+  }
+
+  deleteBookmark = bookmarkId => {
+    const newBookmarks = this.state.bookmarks.filter(bm =>
+      bm.id !== bookmarkId)
+    this.setState({bookmarks: newBookmarks})
   }
 
   componentDidMount() {
@@ -68,29 +76,29 @@ class App extends Component {
   }
 
   render() {
-    const { bookmarks } = this.state
+    const contextValue = {
+      bookmarks: this.state.bookmarks,
+      addBookmark: this.addBookmark,
+      deleteBookmark: this.deleteBookmark,
+    }
     return (
       <main className='App'>
         <h1>Bookmarks!</h1>
+        <BookmarksContext.Provider value={contextValue}>
         <Nav />
         <div className='content' aria-live='polite'>
           <Route
             path='/add-bookmark'
-            render={({ history }) => {
-              return <AddBookmark
-                onAddBookmark={this.addBookmark}
-                onClickCancel={() => history.push('/')}
-              />
-            }}
+            component={AddBookmark}
           />
           <Route
             exact
             path='/'
-            render={({ history }) => {
-              return <BookmarkList bookmarks={bookmarks} />
-            }}
+            component={BookmarkList}
           />
         </div>
+        
+        </BookmarksContext.Provider>
       </main>
     );
   }
--- src/BookmarkList/BookmarkList.js
+++ src/BookmarkList/BookmarkList.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+import BookmarksContext from '../BookmarksContext';
 import BookmarkItem from '../BookmarkItem/BookmarkItem';
+import PropTypes from 'prop-types';
 import './BookmarkList.css'
 
 class BookmarkList extends Component {
   static defaultProps = {
     bookmarks: []
   };
-
+  static contextType = BookmarksContext;
   render() {
-    const { bookmarks } = this.props
+    const {bookmarks} = this.context;
     return (
       <section className='BookmarkList'>
         <h2>Your bookmarks</h2>
@@ -23,6 +25,15 @@ class BookmarkList extends Component {
       </section>
     );
   }
+}
+
+BookmarkList.prototypes = {
+  bookmarks: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    rating: PropTypes.number,
+    description: PropTypes.string
+  }))
 }
 
 export default BookmarkList;
